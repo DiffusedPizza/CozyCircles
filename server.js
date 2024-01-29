6b705b6bb37573a59499bc41687a3f8ee0d1a04d
DiffusedPizza <rubenlizardi79@gmail.com>
2024-01-28 18:51:20 -0700

    server files template

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
-require('dotenv').config();
-
-
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const todoRoutes = require('./routes/todos')
 
+require('dotenv').config({path: './config/.env'})
+const port = 3000;
+require('dotenv').config();
+// Passport config
+require('.config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection}),
+    })
+)
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+
+app.use('/', mainRoutes)
+app.use('/todos', todoRoutes)
+
+app.listen(process.env.PORT, ()=>{
+    console.log
+})
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
