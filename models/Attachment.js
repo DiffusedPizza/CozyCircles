2871f3e3663d3cff22038e2cc6e2096cb39f7e56
DiffusedPizza <88430429+DiffusedPizza@users.noreply.github.com>
2024-02-27 16:07:17 -0700

    added attachment schema

---
+++ models/Attachment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const attachmentSchema = new mongoose.Schema({
+  messageId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message',
+    required: true
+  },
+  fileType: {
+    type: String,
+    required: true
+  },
+  fileSize: {
+    type: Number,
+    required: true
+  },
+  fileUrl: {
+    type: String,
+    required: true
+  }
+});
+
+const Attachment = mongoose.model('Attachment', attachmentSchema);
+
+module.exports = Attachment;
